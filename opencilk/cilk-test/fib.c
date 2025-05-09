80f58fcc7a34e9e96e2df20f14e38c1d78c05c63
Marc Fischer <mfischer@student.ethz.ch>
2025-05-09 13:50:01 +0200

    Update opencilk to 2.0.1

--- opencilk/cilk-test/fib.c
+++ opencilk/cilk-test/fib.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <cilk/cilk.h>
 
-int fib(int n) {
+uint64_t fib(uint64_t n) {
     if (n < 2) return n;
 
-    int x = cilk_spawn fib(n-1);
-    int y = fib(n-2);
-    cilk_sync;
+    uint64_t x, y;
+    cilk_scope {
+        x = cilk_spawn fib(n-1);
+        y = fib(n-2);
+    }
 
     return x + y;
 }
 
 int main(int argc, char* argv[]) {
-    int n = 10;
-    int result = fib(n);
-    printf("fib(%d) = %d\n", n, result);
+    uint64_t n = 40;
+    uint64_t result = fib(n);
+    printf("fib(%" PRIu64 ") = %" PRIu64 "\n", n, result);
 
     return 0;
 }
